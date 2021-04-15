b2957134f7a6b82824ae22b4428db36736f049fa
Jarrison Cano <jarrison.cano@hotmail.com>
2021-04-15 10:20:14 -0500

    Migration to React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import './styles/normalize.css'
+import './styles/globals.css'
 import { useEffect, useState, useRef } from 'react'
-import Head from 'next/head'
-import { Loading } from '../components/Loading/Loading.jsx'
-import { Header } from '../components/Header/Header'
-import { Card } from '../components/Card/Card'
-import { Aside } from '../components/Aside/Aside'
-import { GlobalContext } from '../context/GlobalContext'
-import { CARTAS_URI } from '../data/cartas.json'
+import { Loading } from './components/Loading/Loading.jsx'
+import { Header } from './components/Header/Header'
+import { Card } from './components/Card/Card'
+import { Aside } from './components/Aside/Aside'
+import { GlobalContext } from './context/GlobalContext'
+import { CARTAS_URI } from './data/cartas.json'
 
-const index = () => {
+function App() {
   const [data, setData] = useState(CARTAS_URI)
   const [optionToken, setOptionToken] = useState(true)
   const [loadingToken, setLoadingToken] = useState(true)
@@ -21,10 +22,6 @@ const index = () => {
 
   return (
     <>
-      <Head>
-        <title>Keiken | Heladería </title>
-        <link rel='shortcut icon' href='/img/favicon.jpg' type='image/x-icon' />
-      </Head>
       <GlobalContext.Provider
         value={{ optionToken, setOptionToken, data, setData, sliderContainer }}
       >
@@ -38,4 +35,5 @@ const index = () => {
     </>
   )
 }
-export default index
+
+export default App

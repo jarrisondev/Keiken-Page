1960ed592d89974ff8e04fb69a4853640a8997c7
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-11 00:03:23 -0500

    Improve the Loader animation

--- src/App.js
+++ src/App.js
@@ -8,29 +8,29 @@ import { Aside } from './components/Aside/Aside'
 import { GlobalContext } from './context/GlobalContext'
 import { CARTAS_URI } from './data/cartas.json'
 
-function App () {
-  const [data, setData] = useState(CARTAS_URI)
-  const [loadingToken, setLoadingToken] = useState(true)
-  const sliderContainer = useRef(null)
+function App() {
+	const [data, setData] = useState(CARTAS_URI)
+	const [loadingToken, setLoadingToken] = useState(true)
+	const sliderContainer = useRef(null)
 
-  useEffect(() => {
-    setInterval(() => {
-      setLoadingToken(false)
-    }, 4000)
-  }, [])
+	useEffect(() => {
+		setInterval(() => {
+			setLoadingToken(false)
+		}, 4000)
+	}, [])
 
-  return (
-    <>
-      <GlobalContext.Provider value={{ data, setData, sliderContainer }}>
-        {loadingToken && <Loading />}
-        <div className='container'>
-          <Header />
-          <Card />
-          <Aside />
-        </div>
-      </GlobalContext.Provider>
-    </>
-  )
+	return (
+		<>
+			<GlobalContext.Provider value={{ data, setData, sliderContainer }}>
+				<Loading loadingToken={loadingToken} />
+				<div className='container'>
+					<Header />
+					<Card />
+					<Aside />
+				</div>
+			</GlobalContext.Provider>
+		</>
+	)
 }
 
 export default App
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,14 +1,15 @@
-export const Loading = () => {
-  return (
-    <div className='loading-container'>
-      <img src='/img/logo.jpg' alt='Logo de bienvenida' />
-      <br />
-      <div className='container-circles'>
-        <div className='first-circle' />
-        <div className='second-circle' />
-        <div className='third-circle' />
-        <div className='four-circle' />
-      </div>
-    </div>
-  )
+import { LoadingStyled } from './styles'
+export const Loading = ({ loadingToken }) => {
+	return (
+		<LoadingStyled lt={loadingToken}>
+			<img src='/img/logo.jpg' alt='Logo de bienvenida' />
+			<br />
+			<div className='container-circles'>
+				<div className='first-circle' />
+				<div className='second-circle' />
+				<div className='third-circle' />
+				<div className='four-circle' />
+			</div>
+		</LoadingStyled>
+	)
 }
